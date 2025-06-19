1d9ef6bb62aed43017bca7cef3327e15331086de
vinayboss9669 <vinayboss9669@gmail.com>
2025-06-19 16:48:24 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+
+const app = express();
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+mongoose.connect("mongodb://localhost:27017/email-verify")
+  .then(() => {
+    app.listen(5000, () => console.log("Server started at http://localhost:5000"));
+  });
+app.listend(8080,()=>{
+    console.log("server are run port 8080");
+}
